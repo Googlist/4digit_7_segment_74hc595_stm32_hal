33b0ff086976b9469fa466dc454590138debf947
Googlist <googlist@zzz.net>
2021-05-18 03:16:01 +0200

    initial commit

--- d4s7_74hc595.c
+++ d4s7_74hc595.c
@@ -2,17 +2,27 @@
 
 #define NumSpi SPI2
 
-unsigned const char digit[10] =
+unsigned char digit[10] =
     {
     0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90
     //numbers
 	};
-unsigned const char column[4] =
+unsigned char column[4] =
     {
     0x01, 0x02, 0x04, 0x08
     //positions
-    //,0x10,0x20,0x40,0x80   //dots??
+    //,0x10,0x20,0x40,0x80  //8-digits position??
 	};
+
+unsigned char digit_pointed[10] =
+    {
+    0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x10
+    };
+
+unsigned char letters[6] =
+    {
+    0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e
+    }; //A, b, C, d, E, F
 
 void Set595IntNumber(SPI_HandleTypeDef *hspi, uint16_t value);
 void Set595IntNumber(SPI_HandleTypeDef *hspi, uint16_t value)
@@ -57,6 +67,7 @@ void Set595IntNumber(SPI_HandleTypeDef *hspi, uint16_t value)
 	buff[0] = digit[digit_buf[count]];	     	//numero
 
 	HAL_SPI_Transmit(hspi, (uint8_t*) &buff, 2, 10);
+	//HAL_SPI_Transmit_DMA(hspi, (uint8_t*) &buff, 2);
 
 #ifdef CS_Soft
 
